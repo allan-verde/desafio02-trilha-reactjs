8d8900b28b44f5f3472c8a4f80adae5b79cd1f05
Allan Verde <allangreen2906@gmail.com>
2023-08-14 08:25:28 -0300

    feat: base of styled-components

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-900': '#FAFAFA',
+  'gray-800': '#F3F2F2',
+  'gray-700': '#EDEDED',
+  'gray-600': '#E6E5E5',
+  'gray-500': '#D7D5D5',
+  'gray-400': '#8D8686',
+  'gray-300': '#574F4D',
+  'gray-200': '#403937',
+  'gray-100': '#272221',
+
+  'yellow-dark': '#C47F17',
+  'yellow': '#DBAC2C',
+  'yellow-light': '#F1E9C9',
+
+  'purple-dark': '#4B2995',
+  'purple': '#8047F8',
+  'purple-light': '#EBE5F9',
+}
+  
